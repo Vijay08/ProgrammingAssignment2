5d47b539830bfe079d67cce1fa916fc53b71c28a
Vijay08 <vjay0890@gmail.com>
2016-04-29 20:21:43 -0500

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    # Conflicts:
    #	README.md
    #	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The assignment is about cache the inverse of matix
+#Rather than computing the inverse of the matrix its better to cache it avoid repeated computation
 
-## Write a short comment describing this function
+#The below function creates a special object matrix to cache the inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_mat <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_mat <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv_mat <<- inverse
+  getInverse <- function() inv_mat
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
+#The below function computes the matrix inverse of the special  matrix from the above function. 
+#The inverse of the matrix is cached if its not the same matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv_mat <- x$getInverse()
+  if (!is.null(inv_mat)) {
+    message("getting cached data")
+    return(inv_mat)
+  }
+  mat <- x$get()
+  inv_mat <- solve(mat, ...)
+  x$setInverse(inv_mat)
+  inv_mat
 }
